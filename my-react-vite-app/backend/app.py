900bba2f78684c325cc6ad29df480934741212c9
mymuna-rahman <mymunarahman03@gmail.com>
2025-01-16 02:49:23 +0600

    fix: Remove tracked files that should be ignored

---
+++ my-react-vite-app/backend/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Flask is running!"
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify({"message": "Hello from Flask!"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
