caae14cb68c7838e20e3e324af5a42d88b9c1d22
mymuna-rahman <mymunarahman03@gmail.com>
2025-01-16 02:59:56 +0600

    del

--- Calculator/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.cs.oop.techgiant.calculator {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.cs.oop.techgiant.calculator to javafx.fxml;
-    exports com.cs.oop.techgiant.calculator;
-}
