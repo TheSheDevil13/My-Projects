eecb4cfec5389a339a483a8581a2234b3a4efd6d
mymuna-rahman <mymunarahman03@gmail.com>
2025-05-20 03:07:24 +0600

    tested useState

--- my-portfolio/frontend/src/components/sections/About.jsx
+++ my-portfolio/frontend/src/components/sections/About.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function About() {
+  const [count, setCount] = useState(0);
   return (
     <div className='page'>
       <h1>About</h1>
       <p>This is my portfolio website. I built it using React.</p>
+      <hr />
+      <button onClick={() => setCount(count+1)}>Pressed {count} times</button>
     </div>
   );
 }
